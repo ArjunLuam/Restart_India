e9351fb287303f7d76efe3ea031d66570ecec708
arjun anand <arjunluam0732@gmail.com>
2020-06-11 02:40:24 +0530

    Verificaton activity updated

---
+++ app/src/main/java/com/restartindia/naukri/login/register_employee.java
@@ -0,0 +1,26 @@
+package com.restartindia.naukri.login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.restartindia.naukri.R;
+
+public class register_employee extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register_employee, container, false);
+    }
+}
