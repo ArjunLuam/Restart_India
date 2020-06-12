8143fd4b94ce3d0459e89f83b91b90d49f32e77e
Divij Gupta <divijgupta66@gmail.com>
2020-06-13 00:17:39 +0530

    Image upload fixed

--- app/src/main/java/com/restartindia/naukri/login/RegisterEmployerFragment.java
+++ app/src/main/java/com/restartindia/naukri/login/RegisterEmployerFragment.java
@@ -1,16 +1,14 @@
 package com.restartindia.naukri.login;
 
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
-import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,11 +19,9 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.restartindia.naukri.R;
-import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -35,11 +31,12 @@ import static android.app.Activity.RESULT_OK;
 public class RegisterEmployerFragment extends Fragment {
 
     CircleImageView circleImageView;
-    public static final int PICK_IMAGE_REQUEST=1;
+    public static final int PICK_IMAGE_REQUEST = 1;
     Uri imageuri;
     private MaterialButton msubmit;
     private StorageReference mstorageref;
-    ProgressBar mprogressbar;
+    ProgressDialog progressDialog;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,15 +45,20 @@ public class RegisterEmployerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_register_employer, container, false);
-        circleImageView=view.findViewById(R.id.ivUploadedImage);
-        msubmit=view.findViewById(R.id.empl_submit);
-        mprogressbar=view.findViewById(R.id.progressBar);
-        mstorageref= FirebaseStorage.getInstance().getReference("profile_pics");
+        View view = inflater.inflate(R.layout.fragment_register_employer, container, false);
+        circleImageView = view.findViewById(R.id.ivUploadedImage);
+        msubmit = view.findViewById(R.id.empl_submit);
+
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setTitle("Registering");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setMessage("Please Wait");
+
+        mstorageref = FirebaseStorage.getInstance().getReference("profile_pics");
         circleImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
                 openfilechooser();
             }
         });
@@ -71,58 +73,52 @@ public class RegisterEmployerFragment extends Fragment {
 
         return view;
     }
+
     private void openfilechooser() {
-        Intent gallery=new Intent();
+        Intent gallery = new Intent();
         gallery.setType("image/*");
         gallery.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(gallery,PICK_IMAGE_REQUEST);
+        startActivityForResult(gallery, PICK_IMAGE_REQUEST);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==PICK_IMAGE_REQUEST && resultCode==RESULT_OK && data !=null && data.getData() !=null){
-            imageuri=data.getData();
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageuri = data.getData();
             circleImageView.setImageURI(imageuri);
         }
     }
-    private String getfileExtension(Uri uri){
-       ContentResolver cr=getActivity().getContentResolver();
-        MimeTypeMap mime=MimeTypeMap.getSingleton();
+
+    private String getfileExtension(Uri uri) {
+        ContentResolver cr = getActivity().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
         return mime.getExtensionFromMimeType(cr.getType(uri));
     }
 
-    private void uploadphoto(){
-        if(imageuri !=null){
-            StorageReference fileref=mstorageref.child(System.currentTimeMillis()
-            + "."+getfileExtension(imageuri));
+    private void uploadphoto() {
+        if (imageuri != null) {
+            progressDialog.show();
+            StorageReference fileref = mstorageref.child(System.currentTimeMillis()
+                    + "." + getfileExtension(imageuri));
 
             fileref.putFile(imageuri)
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-
-                                    mprogressbar.setProgress(0);
-
-                            Toast.makeText(getContext(),"Upload Sucessful",Toast.LENGTH_LONG).show();
+                            Toast.makeText(getContext(), "Upload Successful", Toast.LENGTH_LONG).show();
+                            progressDialog.dismiss();
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            double progress=(100 *(taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount()));
-                            mprogressbar.setProgress((int) progress);
+                            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
                         }
                     });
-        }else{
-            Toast.makeText(getContext(),"Upload photo",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getContext(), "Upload photo", Toast.LENGTH_SHORT).show();
         }
 
     }
