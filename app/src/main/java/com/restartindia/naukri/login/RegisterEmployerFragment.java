539c9469cc6e565d1c03c13fbe11409e3043d48d
arjun anand <arjunluam0732@gmail.com>
2020-06-13 19:54:04 +0530

    Geolocation added

--- app/src/main/java/com/restartindia/naukri/login/RegisterEmployerFragment.java
+++ app/src/main/java/com/restartindia/naukri/login/RegisterEmployerFragment.java
@@ -1,10 +1,13 @@
 package com.restartindia.naukri.login;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,10 +18,15 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -28,7 +36,9 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.restartindia.naukri.R;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -88,6 +98,31 @@ public class RegisterEmployerFragment extends Fragment {
         progressDialog.setMessage("Please Wait");
 
         mstorageref = FirebaseStorage.getInstance().getReference("profile_pics");
+
+        //Geolocation
+        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(getContext());
+        if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location=task.getResult();
+                    if(location !=null){
+                        Geocoder geocoder=new Geocoder(getActivity(), Locale.getDefault());
+                        try {
+                            addressList=geocoder.getFromLocation(
+                                    location.getLatitude(),location.getLongitude(),1
+                            );
+                            etDistrict.setText(addressList.get(0).getLocality());
+                            etCity.setText(addressList.get(0).getPostalCode());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        }else{
+            ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST_CODE);
+        }
         circleImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
