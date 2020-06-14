5520295b46725e86294f5e5a0e9180fb5b6d9001
arjun anand <arjunluam0732@gmail.com>
2020-06-14 12:51:02 +0530

    dialogue added

---
+++ app/src/main/java/com/restartindia/Dialog.java
@@ -0,0 +1,16 @@
+package com.restartindia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.restartindia.naukri.R;
+
+public class Dialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+    }
+}
